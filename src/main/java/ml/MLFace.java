5fe4f6cea5103239a5664e8e11ed81632f54435f
Jeongin Lee <jeonginlee0304@gmail.com>
2022-09-10 15:03:49 -0400

    rename landmarks to keyPoints and add functions to retrieve each facial keypoint

--- src/main/java/ml/MLFace.java
+++ src/main/java/ml/MLFace.java
@@ -7,19 +7,39 @@ import ml.MLObject;
 import ml.MLKeyPoint;
 
 public class MLFace extends MLObject {
-    List<MLKeyPoint> landmarks;
+    List<MLKeyPoint> keyPoints;
 
-    public MLFace(String label, float confidence, float x, float y, float width, float height, List<MLKeyPoint> landmarks) {
+    public MLFace(String label, float confidence, float x, float y, float width, float height, List<MLKeyPoint> keyPoints) {
         super(label, confidence, x, y, width, height);
-        this.landmarks = landmarks;
+        this.keyPoints = keyPoints;
     }
 
-    public MLFace(float confidence, float x, float y, float width, float height, List<MLKeyPoint> landmarks) {
+    public MLFace(float confidence, float x, float y, float width, float height, List<MLKeyPoint> keyPoints) {
         super("Face", confidence, x, y, width, height);
-        this.landmarks = landmarks;
+        this.keyPoints = keyPoints;
     }
 
-    public List<MLKeyPoint> getLandmarks() {
-        return this.landmarks;
+    public List<MLKeyPoint> getKeyPoints() {
+        return this.keyPoints;
+    }
+
+    public MLKeyPoint getLeftEye() {
+        return this.keyPoints.get(0);
+    }
+
+    public MLKeyPoint getRightEye() {
+        return this.keyPoints.get(1);
+    }
+
+    public MLKeyPoint getNose() {
+        return this.keyPoints.get(2);
+    }
+
+    public MLKeyPoint getLeftMouth() {
+        return this.keyPoints.get(3);
+    }
+
+    public MLKeyPoint getRightMouth() {
+        return this.keyPoints.get(4);
     }
 }
