21bf56ad2b3b6389544a251cf4373f846db2b367
Jeongin Lee <jeonginlee0304@gmail.com>
2022-09-10 15:04:30 -0400

    follow naming convention

--- src/main/java/ml/MLPose.java
+++ src/main/java/ml/MLPose.java
@@ -4,81 +4,81 @@ import ml.MLKeyPoint;
 import java.util.List;
 
 public class MLPose {
-    private List<MLKeyPoint> keypoints;
+    private List<MLKeyPoint> keyPoints;
 
-    public MLPose(List<MLKeyPoint> keypoints) {
-        this.keypoints = keypoints;
+    public MLPose(List<MLKeyPoint> keyPoints) {
+        this.keyPoints = keyPoints;
     }
 
     public List<MLKeyPoint> getKeyPoints() {
-        return this.keypoints;
+        return this.keyPoints;
     }
 
     public MLKeyPoint getNose() {
-        return this.keypoints.get(0);
+        return this.keyPoints.get(0);
     }
 
     public MLKeyPoint getLeftEye() {
-        return this.keypoints.get(1);
+        return this.keyPoints.get(1);
     }
 
     public MLKeyPoint getRightEye() {
-        return this.keypoints.get(2);
+        return this.keyPoints.get(2);
     }
 
     public MLKeyPoint getLeftEar() {
-        return this.keypoints.get(3);
+        return this.keyPoints.get(3);
     }
 
     public MLKeyPoint getRightEar() {
-        return this.keypoints.get(4);
+        return this.keyPoints.get(4);
     }
 
     public MLKeyPoint getLeftShoulder() {
-        return this.keypoints.get(5);
+        return this.keyPoints.get(5);
     }
 
     public MLKeyPoint getRightShoulder() {
-        return this.keypoints.get(6);
+        return this.keyPoints.get(6);
     }
 
     public MLKeyPoint getLeftElbow() {
-        return this.keypoints.get(7);
+        return this.keyPoints.get(7);
     }
 
     public MLKeyPoint getRightElbow() {
-        return this.keypoints.get(8);
+        return this.keyPoints.get(8);
     }
 
     public MLKeyPoint getLeftWrist() {
-        return this.keypoints.get(9);
+        return this.keyPoints.get(9);
     }
 
     public MLKeyPoint getRightWrist() {
-        return this.keypoints.get(10);
+        return this.keyPoints.get(10);
     }
 
     public MLKeyPoint getLeftHip() {
-        return this.keypoints.get(11);
+        return this.keyPoints.get(11);
     }
 
     public MLKeyPoint getRightHip() {
-        return this.keypoints.get(12);
+        return this.keyPoints.get(12);
     }
 
     public MLKeyPoint getLeftKnee() {
-        return this.keypoints.get(13);
+        return this.keyPoints.get(13);
     }
 
     public MLKeyPoint getRightKnee() {
-        return this.keypoints.get(14);
+        return this.keyPoints.get(14);
     }
 
     public MLKeyPoint getLeftAnkle() {
-        return this.keypoints.get(15);
+        return this.keyPoints.get(15);
     }
 
     public MLKeyPoint getRightAnkle() {
-        return this.keypoints.get(16);
+        return this.keyPoints.get(16);
     }
 }
